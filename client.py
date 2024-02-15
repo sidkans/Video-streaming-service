e35fd99a9d7b19e0b75d61c43b9aa8ef4224cecf
Shiva4113 <shivagolugula123@gmail.com>
2024-02-15 21:34:07 +0530

    Client changes

--- client.py
+++ client.py
@@ -2,42 +2,58 @@
 import socket
 import cv2
 import pickle
-
+import struct
+import numpy as np
 
 #constants
 HOST_IP = "192.168.252.240"
+CLIENT_NAME = socket.gethostname()
 PORT = 8080
 ADDR = (HOST_IP,PORT)
 FORMAT = "utf-8"
-
+WIDTH = 640
+HEIGHT = 400
+PAYLOAD_SIZE = 1024
 
 #client
-
 def run_client():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
-    cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print(f"[SERVER]\nHOST IP:{HOST_IP}\n\n")
+    
+    # cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cam = cv2.VideoCapture("./JigsAcademyTesting.mp4")
+    print(f"[CAMERA] Turning on Camera...",end= ' ')
+
     if cam.isOpened():
+        print("SUCCESS!")
         try:
             while True:
                 ret, frame = cam.read()
                 if ret:
-                    cv2.imshow('my frame', frame)
+                    cv2.imshow(f'{CLIENT_NAME}', frame)
+                    frame = cv2.resize(frame,(WIDTH,HEIGHT))
                     key = cv2.waitKey(1) &  0xFF
-                    if key == ord('q') or key == 27:
-                        cam.release()
-                        cv2.destroyAllWindows()
-                   
+                    if key == ord('Q') or key == 27:
+                        break
+
+                    #serialization of frames first
+                    serialized_frames = pickle.dumps(frame)
+                    frame_size = len(serialized_frames)
+                    packet_count = int(np.ceil(frame_size/PAYLOAD_SIZE))
+
+                    for i in range(packet_count):
+                        start = i*PAYLOAD_SIZE
+                        end = min((i+1)*PAYLOAD_SIZE,frame_size)
+                        packet = serialized_frames[start:end]
+                        packet_encoded = struct.pack('!L',i)+packet
+                        client.sendto(packet_encoded,ADDR)
+                    # print("Sending frames") #success!!!!
         finally:
             # Release the camera and destroy all windows here
-            # cam.release()
-            # cv2.destroyAllWindows()
-            pass
+            cam.release()
+            cv2.destroyAllWindows()
+            
 
     client.close()
-
-run_client()
-
-
 
 run_client()
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 #imports
 import socket
 import threading
-import cv2
-import pickle
-import imutils
-import time
+# import cv2
+# import pickle
+# import imutils
+# import time
 import struct
 
 #constants
@@ -13,26 +13,72 @@ HOST_IP = socket.gethostbyname(HOST)
 PORT = 8080
 ADDR = (HOST_IP,PORT)
 FORMAT = "utf-8"
-PAYLOAD_SIZE = struct.calcsize("Q")#Q is format for long long int btw --> 8 bytes on most platforms
+PAYLOAD_SIZE = 1024
+THREAD_COUNT = -1
+MAX_THREADS = 5
+
+
+def handle_client(server):
+    while True:
+        data,addr = server.recvfrom(PAYLOAD_SIZE)
+        if not data:
+            print("[DISCONNECTED] Client Disconnected")
+            break
+        else:
+            print(f"Received messages from {addr}:{data}")
+
 
 #server
 def start_server():
-    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    print(f"[SERVER]\nHOST:{HOST}\nHOST IP:{HOST_IP}\n\n")
-    server.bind(ADDR)
-    print("[BIND] Success!\n")
-    server.listen(5)
-    print(f"[LISTENING] on PORT NO:{PORT}\n")
+    global THREAD_COUNT
     
-    while True:
-        conn,addr = server.accept()
-        print(f"[CONNECTION] New Connection: {addr}")
-        # if conn:
-        #     vid = cv2.VideoCapture(0)
-        #     while vid.isOpened():
-        #         pass
-            
-            
+    server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    print(f"[SERVER]\nHOST:{HOST}\nHOST IP:{HOST_IP}\n\n")
+    
+    try:
+        server.bind(ADDR)
+        print("[BIND] Success!\n")
+    except socket.error as err:
+        print(str(err))
+    
+    
+
+    thread = threading.Thread(target=handle_client, args=(server,))
+    thread.start()
+    print(f"[ACTIVE THREADS]: {THREAD_COUNT}")
+        
+
+
+
+# def thread_clients(server):
+#     global THREAD_COUNT
+    
+#     while True:
+#         conn,addr = server.accept()
+#         THREAD_COUNT+=1
+#         if THREAD_COUNT >= MAX_THREADS:
+#             print("[LIMIT] Maximum Number of Clients reached! Closing connection ...\n")
+#             conn.close()
+#             continue
+        
+#         # THREAD_COUNT+=1
+#         # print(f"[ACTIVE THREADS]:{THREAD_COUNT}")
+
+#         try:
+#             while True:
+#                 data = conn.recv(PAYLOAD_SIZE)
+#                 if not data:
+#                     break
+                
+        
+#         except socket.error as err:
+#             print(f"[EXCEPTION] {err}")
+        
+#         finally:
+#             THREAD_COUNT-=1
+#             conn.close()
+#             print(f"[DISCONNECTED] Client Disconnected.\n[ACTIVE THREADS]:{THREAD_COUNT}")
+
 
         
 start_server()
