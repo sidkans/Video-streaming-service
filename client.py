0acb7e4b2aaef326ee03c0821a101a77084cf28e
Shiva4113 <shivagolugula123@gmail.com>
2024-02-22 13:29:48 +0530

    sender-receiver-final

--- client.py
+++ client.py
@@ -2,11 +2,9 @@
 import socket
 import cv2
 import pickle
-import struct
-import numpy as np
 
 #constants
-HOST_IP = "10.1.1.151"
+HOST_IP = "192.168.252.240"
 CLIENT_NAME = socket.gethostname()
 PORT = 8080
 ADDR = (HOST_IP,PORT)
@@ -16,70 +14,28 @@ HEIGHT = 400
 PAYLOAD_SIZE = 1024
 
 
-
-# Client
-def close_client_socket(client_socket):
-    try:
-        # Send a termination message to the server
-        client_socket.sendto(b'', ADDR)
-    except socket.error as err:
-        print(f"[ERROR] Failed to send termination message: {err}")
-    finally:
-        # Close the socket
-        client_socket.close()
-
-
 # client
 def run_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client.bind(ADDR)
     print(f"[CLIENT]\nHOST IP:{HOST_IP}\n\n")
 
-    print(f"[CAMERA] Turning on Camera...", end=' ') 
-    cam = cv2.VideoCapture(0)
-    
-
-    try:
-        '''
-        # while cam.isOpened():
-        #     # print("SUCCESS!")
-        #     ret, frame = cam.read()
-        #     if not ret:
-        #         break
-            
-        #     cv2.imshow(f'{CLIENT_NAME}', frame)
-        #     frame = cv2.resize(frame, (WIDTH, HEIGHT))
-        #     key = cv2.waitKey(20) & 0xFF
-        #     if key == ord('Q') or key == 27:
-        #         break
-
-        #     # Serialization of frames first
-        #     serialized_frames = pickle.dumps(frame)
-        #     frame_size = len(serialized_frames)
-        #     packet_count = int(np.ceil(frame_size / PAYLOAD_SIZE))
-
-        #     for i in range(packet_count):
-        #         start = i * PAYLOAD_SIZE
-        #         end = min((i + 1) * PAYLOAD_SIZE, frame_size)
-        #         packet = serialized_frames[start:end]
-        #         packet_encoded = struct.pack('!L', i) + packet
-        #         client.sendto(packet_encoded, ADDR)
-        #         # print("Sending frames")  # success!!!!'''
-        while cam.isOpened():
-            ret,photo = cam.read()
-            cv2.imshow('name',photo)
-            ret, buffer= cv2.imencode(".jpg",photo,[int(cv2.IMWRITE_JPEG_QUALITY),30])
-            x_bytes = pickle.dumps(buffer)
-            client.sendto(x_bytes,ADDR)
+    while True:
+        try:
+            data, server_address = client.recvfrom(65507)
+            if not data:
+                break
+            data = pickle.loads(data)
+            data = cv2.imdecode(data, cv2.IMREAD_COLOR)
+            cv2.imshow('CLIENT', data)
             if cv2.waitKey(10) == 13:
                 break
-            pass
+        except OSError as e:
+            print(f"Error: {e}")
 
-    finally:
-        # Release the camera and destroy all windows here
-        cam.release()
-        cv2.destroyAllWindows()
-
-    close_client_socket(client)
+    client.close()
 
 run_client()
+
+
 
--- server.py
+++ server.py
@@ -1,11 +1,8 @@
 #imports
 import socket
-# import threading
 import cv2
 import pickle
-# import imutils
 import time
-import struct
 
 #constants
 HOST = socket.gethostname()
@@ -14,103 +11,44 @@ PORT = 8080
 ADDR = (HOST_IP,PORT)
 FORMAT = "utf-8"
 PAYLOAD_SIZE = 2048
-# THREAD_COUNT = 0
-# MAX_THREADS = 5
-MAX_CONNECTIONS =  5
-
-
-#accept gracious termination
 
 
 #server
 def start_server():
-    # global THREAD_COUNT
     
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
     print(f"[SERVER]\nHOST:{HOST}\nHOST IP:{HOST_IP}\n\n")
     
     try:
-        server.bind(ADDR)
+        # server.bind(ADDR)
         print("[BIND] Success!\n")
     except socket.error as err:
         print(str(err))
         return
     
-    print("[WAITING] Waiting for New Connections ...\n")
-    print(f"[LISTENING] on PORT NO:{PORT}\n")
+    cam = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    try:
+        while cam.isOpened():
+            ret,photo = cam.read()
+            cv2.imshow('SERVER',photo)
+            ret, buffer= cv2.imencode(".jpg",photo,[int(cv2.IMWRITE_JPEG_QUALITY),30])
+            x_bytes = pickle.dumps(buffer)
+            server.sendto(x_bytes,ADDR)
+            if cv2.waitKey(10) == 13:
+                print("[STREAM] Stream is ending ...")
+                time.sleep(2)
+                break
     
-    client_data = {}
-    connection_count =  0
-    client_connections = {}
+    except Warning:
+        pass
 
-    while True:
-        '''# try:
-        #     data, addr = server.recvfrom(PAYLOAD_SIZE)
-        # except ConnectionResetError:
-        #     # print(f"[CONNECTION RESET] {addr} has disconnected.")
-        #     print("No of connections:",connection_count)
-            
-        
-        # if not data:
-        #     # Client has disconnected
-        #     if addr in client_connections and client_connections[addr]:
-        #         client_connections[addr] = False
-        #         # print(f"[DISCONNECTED] {addr} has disconnected.")
-        #         connection_count -= 1
-        #     continue
-        
-        
-        # if addr not in client_connections:
-        #     client_connections[addr] = True
-        #     connection_count += 1
-        # elif client_connections.get(addr, False) == False:
-        #     print(f"[RECONNECTED] {addr} has reconnected.")
-        #     client_connections[addr] = True
-        #     connection_count += 1
-            
-            
-        # #deserealizing frames
-        # packet_init = struct.unpack("!L",data[0:4])[0]
-        # packet_decoded = data[4:]
-        # if packet_init ==0:
-        #     frame = packet_decoded
-        # else:
-        #     frame+=packet_decoded
-        # try:
-        #     if packet_init ==0 or len(frame) == PAYLOAD_SIZE:
-        #         frame = pickle.loads(frame)
-        #         frame = cv2.imdecode(frame,cv2.IMREAD_COLOR)
-        #         cv2.imshow("Receiving",frame)
-        #         cv2.waitKey(20)
-        # except pickle.UnpicklingError as err:
-        #     # print(f"Unable to deserealize frame:{err}")
-        #     pass
+    finally:
+        # Release the camera and destroy all windows 
+        cam.release()
+        cv2.destroyAllWindows()
+        print(f"[TERMINATING] SERVER IS CLOSING !")
+        time.sleep(1)
 
-        # if connection_count >= MAX_CONNECTIONS:
-        #     print(f"[LIMIT REACHED] Maximum number of connections ({MAX_CONNECTIONS}) reached.")
-        #     continue
-        
-        # if addr not in client_data:
-        #     print(f"[NEW CONNECTION] from {addr}.\n")
-        #     client_data[addr] = []
-        #     client_connections[addr] = True
-        # client_data[addr].append(data)
-
-        # # print(f"Received message: {data} from {addr}")
-        
-
-        # for client_addr, client_messages in client_data.items():
-        #     if client_connections.get(client_addr, False):
-        #         for message in client_messages:
-        #             server.sendto(message, client_addr)
-        '''
-        x = server.recvfrom(65507)
-        clientip = x[1][0]
-        data = x[0]
-        data = pickle.loads(data)
-        data = cv2.imdecode(data,cv2.IMREAD_COLOR)
-        cv2.imshow('name2',data)
-        if cv2.waitKey(10) == 13:
-            break
+    server.close()
 
 start_server()
